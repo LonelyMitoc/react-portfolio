b0c9c8aea00d8e084fb5d58f726f8fd2efbdb170
Triglyceride <jamesmatsu@gmail.com>
2023-01-18 12:22:14 -0800

    add to About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 export default function About() {
-  return {
+  return (
     <section id="about">
-      
+      <div>
+        <div>
+          <h1>Hi! My name is James.
+          <br className="hidden" />I love to build apps that help improve quality of life.
+          </h1>
+          <p>
+            
+          </p>
+        </div>
+      </div>
     </section>
-  }
+  )
 }
