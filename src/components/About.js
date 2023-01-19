74da66edee347befc66029e239312b73ad495db2
Triglyceride <jamesmatsu@gmail.com>
2023-01-18 16:45:35 -0800

    update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 export default function About() {
   return (
-    <section id="about">
+    <section id="about-me">
       <div>
         <div>
           <h1>Hi! My name is James.
           <br className="hidden" />I love to build apps that help improve quality of life.
           </h1>
           <p>
-            
+            I was born and raised in Bellevue, Washington. I went to the University of Washington and graduated with a BA degree in Biochemistry but at the time I was burnt out on studying and working in my field so I started working in the logistics industry. After a few years, I felt I needed a career change as well as realizing there were many aspects in my daily operation that could be automated and wanted to create something that could achieve that so I decided to enroll in the Full Stack Coding Bootcamp through the University of Washington to jump into the world of coding.
           </p>
         </div>
       </div>
