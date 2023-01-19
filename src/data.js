6f75a51b88ac1d5ca809140caeec3afb26ef543a
LonelyMitoc <jamesmatsu@gmail.com>
2023-01-18 20:52:08 -0800

    project data input

---
+++ src/data.js
@@ -0,0 +1,49 @@
+export const projectData = [
+  {
+    title: "Boundless",
+    alt: "Boundless",
+    page: "https://boundless-stories.herokuapp.com/",
+    repo: "https://github.com/LonelyMitoc/boundless"
+  },
+  {
+    title: "Movie Stream Availability Search Tool",
+    alt: "movie streaming availability search engine",
+    page: "https://lonelymitoc.github.io/moviestreamAvail/",
+    repo: "https://github.com/LonelyMitoc/moviestreamAvail"
+  },
+  {
+    title: "Employee Manager",
+    alt: "employee manager / tracker",
+    repo: "https://github.com/LonelyMitoc/track-your-employees"
+  },
+  {
+    title: "Weather Dashboard",
+    alt: "weather dashboard",
+    page: "https://lonelymitoc.github.io/tempestquery/",
+    repo: "https://github.com/LonelyMitoc/tempestquery"
+  },
+  {
+    title: "Project Team Member Card Generator",
+    alt: "project team member info generator",
+    repo: "https://github.com/LonelyMitoc/project-member-card"
+  },
+];
+
+export const frontEnd = [
+  "Bootstrap",
+  "Bulma",
+  "JavaScript",
+  "JQuery",
+  "REACT"
+];
+
+export const backEnd = [
+  "REST APIs",
+  "Node",
+  "OOP",
+  "Express",
+  "MySQL",
+  "Sequelize",
+  "MongoDB",
+  "Mongoose",
+];
