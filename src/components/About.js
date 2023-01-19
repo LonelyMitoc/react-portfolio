d37ee816c9153f363e3dabc2882b9551cf1356df
LonelyMitoc <jamesmatsu@gmail.com>
2023-01-18 22:01:50 -0800

    update about page

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ export default function About() {
           <br className="hidden" />I love to build apps that help improve quality of life.
           </h1>
           <p>
-            I was born and raised in Bellevue, Washington. I went to the University of Washington and graduated with a BA degree in Biochemistry but at the time I was burnt out on studying and working in my field so I started working in the logistics industry. After a few years, I felt I needed a career change as well as realizing there were many aspects in my daily operation that could be automated and wanted to create something that could achieve that so I decided to enroll in the Full Stack Coding Bootcamp through the University of Washington to jump into the world of coding.
+            I am a Full Stack Developer currently enrolled in the Full Stack Coding Bootcamp through the University of Washington. I enjoy solving complex problems and improving quality of life.
           </p>
         </div>
       </div>
