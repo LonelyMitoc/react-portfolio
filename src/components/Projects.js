86bb004b5b3920218402540123df4e636de85746
LonelyMitoc <jamesmatsu@gmail.com>
2023-01-18 23:59:18 -0800

    partial commit

---
+++ src/components/Projects.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Project = ({ proj }) => {
+  return (
+    <div className="project col-11 col-md-5 col-lg-3">
+      <h3 className="text-center mt-2">{proj.title}</h3>
+      
+    </div>
+  )
+}
